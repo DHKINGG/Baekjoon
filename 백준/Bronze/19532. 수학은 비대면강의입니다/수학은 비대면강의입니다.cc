fd32379e04df957a449527f3e3b14a5ba15cde3d
DHKINGG <110882550+DHKINGG@users.noreply.github.com>
2023-05-14 21:45:58 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -12,18 +12,14 @@ int main() {
 	cout.tie(NULL);
 
 	cin >> a >> b >> c >> d >> e >> f;
-	
+
 	for (int i = -999; i <= 999; i++) {
 		for (int j = -999; j <= 999; j++) {
-			if ((i * a + j * b == c) && (i * d + j * e == f)) {
+			if ((a * i + b * j == c) && (d * i + e * j == f)) {
 				cout << i << " " << j;
 				return 0;
 			}
 			else continue;
 		}
 	}
-
-
-	
-	
 }
