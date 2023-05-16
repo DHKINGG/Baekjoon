4775f2172c119b3d33de0f0ca224bb4753a0b668
DHKINGG <110882550+DHKINGG@users.noreply.github.com>
2023-05-16 20:28:20 +0900

    [Bronze I] Title: 유레카 이론, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.cc
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.cc
@@ -11,28 +11,28 @@ int main() {
 	cin.tie(0);
 	cout.tie(0);
 
-    int n; vector<int> v;
-    cin >> n;
+	 int n; vector<int> v;
+     cin >> n;
 
-    for (int i = 1; i <= 45; i++) {
-        v.push_back(i * (i + 1) / 2);
+    for(int i = 1; i <= 45; i ++){
+        v.push_back(i*(i + 1)/2);
     }
 
-    for (int cnt = 0; cnt < n; cnt++) {
+    for(int cnt = 0; cnt < n; cnt++){
         int count = 0, data;
         cin >> data;
 
-        for (int i = 0; i < v.size(); i++) {
-            for (int j = 0; j < v.size(); j++) {
-                for (int k = 0; k < v.size(); k++) {
-                    if (v[i] + v[j] + v[k] == data) {
+        for(int i = 0; i < v.size(); i++){
+            for(int j = 0; j < v.size(); j++){
+                for(int k = 0; k < v.size(); k++){
+                    if(v[i] + v[j] + v[k] == data){
                         count++;
                         break;
                     }
                 }
-                if (count) break;
+                if(count) break;
             }
-            if (count) break;
+            if(count) break;
         }
 
         cout << count << "\n";
