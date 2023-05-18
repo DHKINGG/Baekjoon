5f9b9908330a19d100cf6051924cc734677372c1
DHKINGG <110882550+DHKINGG@users.noreply.github.com>
2023-05-18 23:19:23 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -0,0 +1,72 @@
+#include <iostream> 
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n, m, k;
+int arr[51][51];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+bool check[51][51];
+
+
+void dfs(int x, int y) {
+	check[x][y] = true;
+
+	for (int dir = 0; dir < 4; dir++) {
+		
+		int nx = x + dx[dir];
+		int ny = y + dy[dir];
+
+		if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+		if (check[nx][ny]) continue;
+		if (arr[nx][ny] != 1)continue;
+
+		dfs(nx, ny);
+	}
+}
+
+
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		for (int i = 0; i < 51; i++) {
+			for (int j = 0; j < 51; j++) {
+				arr[i][j] = 0;
+				check[i][j] = false;
+			}
+		}
+		cin >> m >> n >> k;
+		int cnt = 0;
+
+		while (k--) {
+			int x, y;
+			cin >> x >> y;
+			arr[x][y] = 1;
+
+		}
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][j] == 1 && !check[i][j]) {
+					cnt++;
+					dfs(i, j);
+				}
+			}
+		}
+		cout << cnt << "\n";
+	}
+	
+
+	return 0;
+}
