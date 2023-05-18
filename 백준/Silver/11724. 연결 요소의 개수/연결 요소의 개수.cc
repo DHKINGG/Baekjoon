f5a8c27bd3b28bd8985b41bb08e6e7d81048dc10
DHKINGG <110882550+DHKINGG@users.noreply.github.com>
2023-05-19 00:06:16 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 92 ms, Memory: 6400 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,6 +1,6 @@
-#include <iostream>
+#include <iostream> 
+#include <string>
 #include <algorithm>
-#include <stack>
 #include <vector>
 #include <queue>
 using namespace std;
@@ -10,16 +10,24 @@ vector<int> v[1001];
 bool check[1001];
 
 void dfs(int x) {
-    check[x] = true;
-    
-    for (int i = 0; i < v[x].size(); i++) {
-        int nx = v[x][i];
-        if (check[nx]) continue;
-        dfs(nx);
-    }
+	check[x] = true;
+
+	for (int i = 0; i < v[x].size(); i++) {
+		int nx = v[x][i];
+		if (check[nx]) continue;
+		dfs(nx);
+	}
 }
 
+
+
+
+
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
     for (int i = 0; i < 1001; i++) {
         v[i].clear();
     }
@@ -37,6 +45,10 @@ int main() {
         dfs(i);
     }
     cout << cnt << "\n";
-    
-    return 0;
+
+
+	
+	
+
+	return 0;
 }
