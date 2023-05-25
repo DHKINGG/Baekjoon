08eae605802cf97f9204d1410e7f8f707f5763aa
DHKINGG <110882550+DHKINGG@users.noreply.github.com>
2023-05-25 22:06:31 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.cc
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.cc
@@ -13,29 +13,25 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
+    
     int n;
     cin >> n;
 
     string s;
     cin >> s;
-
-    
     string result = s;
-   
+
     for (int i = 1; i < n; i++) {
         string k;
         cin >> k;
-
         for (int j = 0; j < s.length(); j++) {
             if (s[j] != k[j]) {
                 result[j] = '?';
             }
         }
-
     }
 
     cout << result << "\n";
- 
+
     return 0;
 }
