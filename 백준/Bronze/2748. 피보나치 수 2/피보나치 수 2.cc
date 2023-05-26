e1f4342d26349a55a147e1c8b46a13c75c46c419
DHKINGG <110882550+DHKINGG@users.noreply.github.com>
2023-05-26 17:50:57 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<vector>
+#include<queue>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int n;
+long long arr[90];
+
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+  
+    cin >> n;
+
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    cout << arr[n] << "\n";
+   
+    return 0;
+}
