de87a86dfdc35ab5267f8e0cd2a7810a4ace590c
DHKINGG <110882550+DHKINGG@users.noreply.github.com>
2023-06-11 22:38:52 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,34 +1,37 @@
-#include <iostream> 
-#include <string>
-#include <algorithm>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
 
-	int arr[101] = {};
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
 	int n, m;
+	int arr[101];
 	int sum = 0;
 
 	cin >> n >> m;
-	
+
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
-		
 
 	for (int i = 0; i < n; i++) {
 		for (int j = i + 1; j < n; j++) {
 			for (int k = j + 1; k < n; k++) {
-				if (arr[i] + arr[j] + arr[k] <=m && arr[i] + arr[j] + arr[k] > sum) {
+				if (arr[i] + arr[j] + arr[k] <= m && arr[i] + arr[j] + arr[k] > sum) {
 					sum = arr[i] + arr[j] + arr[k];
 				}
 			}
 		}
-	} cout << sum << "\n";
+	}
+
+	cout << sum << "\n";
 
 
-	return 0;
 }
