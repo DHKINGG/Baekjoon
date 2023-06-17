5c181dd9de9e94b4b6e5f40a629c7d6ac2215109
DHKINGG <110882550+DHKINGG@users.noreply.github.com>
2023-06-17 23:31:58 +0900

    [level 3] Title: 네트워크, Time: 0.47 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool check[2001];
+
+void dfs(int x,int n , vector<vector<int>> computers) {
+    check[x] = true;
+    
+    for(int i = 0; i < n; i++){
+        if(!check[i] && computers[x][i] == 1){
+            dfs(i,n,computers);
+        }
+    }
+    
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    
+    for(int i = 0; i<n; i++){
+        if(!check[i]) {
+            dfs(i,n,computers);
+            answer++;
+        }
+    }
+    
+    return answer;
+}
