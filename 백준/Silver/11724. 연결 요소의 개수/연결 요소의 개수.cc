d0b33261f1ee5a17343860e575bd4639296d846c
DHKINGG <110882550+DHKINGG@users.noreply.github.com>
2023-07-13 21:58:53 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 84 ms, Memory: 6400 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,15 +1,17 @@
-#include <iostream> 
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <queue>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+
 using namespace std;
 
-int n, m;
+int n,m;
 vector<int> v[1001];
 bool check[1001];
 
+
 void dfs(int x) {
+
 	check[x] = true;
 
 	for (int i = 0; i < v[x].size(); i++) {
@@ -19,36 +21,37 @@ void dfs(int x) {
 	}
 }
 
-
-
-
-
 int main() {
-	ios::sync_with_stdio(0);
+
+	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-    for (int i = 0; i < 1001; i++) {
-        v[i].clear();
-    }
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int a, b;
-        cin >> a >> b;
-        v[a].push_back(b);
-        v[b].push_back(a);
-    }
-    int cnt = 0;
-    for (int i = 1; i <= n; i++) {
-        if (check[i]) continue;
-        cnt++;
-        dfs(i);
-    }
-    cout << cnt << "\n";
+	for (int i = 0; i < 1001; i++) {
+		v[i].clear();
+	}
+	
+	cin >> n >> m;
 
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+
+	int cnt = 0;
+
+	for (int i = 1; i <= n; i++) {
+		if (check[i]) continue;
+		cnt++;
+		dfs(i);
+	}
+	cout << cnt << "\n";
+	
 
 	
-	
-
 	return 0;
+
 }
