d186f83ab60ada332a68fe224c54f9b85cc88ce6
DHKINGG <110882550+DHKINGG@users.noreply.github.com>
2023-08-02 23:14:22 +0900

    [Silver V] Title: 오늘도 졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14582. 오늘도 졌다/오늘도 졌다.cc
+++ 백준/Silver/14582. 오늘도 졌다/오늘도 졌다.cc
@@ -6,36 +6,35 @@
 using namespace std;
 
 
-int jemi[9];
-int gri[9];
+int j[9];
+int s[9];
 
-int jemiscroe = 0;
-int griscore = 0;
 
 
 int main() {
 
+	int jscore = 0;
+	int sscore = 0;
 	int stat = 0;
 
 	for (int i = 0; i < 9; i++) {
-		cin >> jemi[i];
+		cin >> j[i];
 	}
 
 	for (int i = 0; i < 9; i++) {
-		cin >> gri[i];
+		cin >> s[i];
 	}
 
 	for (int i = 0; i < 9; i++) {
-		jemiscroe += jemi[i];
-		if (jemiscroe > griscore) stat = 1;
-		griscore += gri[i];
-		if (jemiscroe < griscore && stat == 1) stat = 2;
+		jscore += j[i];
+		if (jscore > sscore) stat = 1;
+		sscore += s[i];
+		if (jscore < sscore && stat == 1) stat = 2;
 	}
 
 	if (stat == 2) cout << "Yes";
 	else cout << "No";
 	
-
 
 
 
